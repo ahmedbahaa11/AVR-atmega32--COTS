2e75853b1f8ee512e8eb0ac5f16872d32836c08e
MostafaSamirKamel <mostafasameer858@gmail.com>
2024-10-19 01:05:08 +0300

    Update project - traffic-light_by PULL-UP_project

--- program/APP/main.c
+++ program/APP/main.c
@@ -1,26 +1,64 @@
+/*
+ * traffic_light.c
+ *
+ * Created: 10/18/2024 10:06:24 PM
+ * Author : Mostafa Samir
+ */ 
+#define F_CPU 8000000UL
 #include <util/delay.h>
 #include "../LIB/STD_TYPES.h"
 #include "../LIB/BIT_MATH.h"
 #include "../MCAL/DIO/DIO_Interface.h"
-#include "../MCAL/TIMERS/TIMER0/TIMER0_Interface.h"
 #include "../HAL/LED/LED_Interface.h"
-#include "../MCAL/GIE/GIE_Interface.h"
+#include "../HAL/LCD/LCD_Interface.h"
+#include "../MCAL/DIO/DIO_Interface.h"
 
-void ISR_OV(void);
 int main(void)
-{
-	GIE_void_GI_Enable(ON);
-	TIMER0_voidInit();
-	LED_voidInit_Led_Pin(PORTB,PIN0);
-	TIMER0_voidSetCallBack_OVF(ISR_OV,1000);
-	while(1)
-	{
-
-	}
-	return 0 ;
+{	
+	LED_voidInit_Led_Pin (PORTA,PIN0); //RED
+	LED_voidInit_Led_Pin (PORTA,PIN1); //YELLOW
+	LED_voidInit_Led_Pin (PORTA,PIN2); //GREEN
+	
+	// Config_Pull_Up_Pin
+	DIO_voidSetPinDirection(PORTB,PIN0,INPUT);
+	DIO_voidSetPinValue(PORTB,PIN0,HIGH);
+	
+    LCD_voidInit ();
+	LCD_voidGoTo_XY (0,0);
+	LCD_voidWriteString (" Eng :");
+	LCD_voidGoTo_XY (1,0);
+	LCD_voidWriteString ("Mostafa Samirs");
+	 _delay_ms(500);
+    while (1) 
+    {
+		
+		u8 button = DIO_u8GetPinValue(PORTB,PIN0);
+		if (button == 0)
+		{
+			
+				 LED_voidStatus_Led_Pin(PORTA,PIN0,ON);
+				 _delay_ms(500);
+				 LED_voidStatus_Led_Pin(PORTA,PIN0,OFF);
+				 _delay_ms(500);
+				
+				LCD_voidClearDisplay ();
+				LCD_voidWriteString (" RED IS ON");
+				
+		}
+		else
+		{
+			
+			LED_voidStatus_Led_Pin(PORTA,PIN1,ON);
+			_delay_ms(500);
+			LED_voidStatus_Led_Pin(PORTA,PIN1,OFF);
+			_delay_ms(500);
+			
+			LCD_voidClearDisplay ();
+			LCD_voidWriteString (" YELLOW IS ON");
+		}
+		
+		
+		
+    }
 }
 
-void ISR_OV(void)
-{
-	LED_voidToggle_Led_Pin(PORTB,PIN0);
-}
