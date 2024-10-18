797247171eae85e28678b2fd2d00001f5c31757a
MostafaSamirKamel <mostafasameer858@gmail.com>
2024-10-18 20:53:23 +0300

    add protues

---
+++ protues/output/Debug/HAL/KPAD/KPAD_Program.d
@@ -0,0 +1,17 @@
+HAL/KPAD/KPAD_Program.d HAL/KPAD/KPAD_Program.o: \
+ ../HAL/KPAD/KPAD_Program.c ../HAL/KPAD/../../LIB/STD_TYPES.h \
+ ../HAL/KPAD/../../LIB/BIT_MATH.h \
+ ../HAL/KPAD/../../MCAL/DIO/DIO_Interface.h ../HAL/KPAD/KPAD_Interface.h \
+ ../HAL/KPAD/KPAD_Private.h ../HAL/KPAD/KPAD_Config.h
+
+../HAL/KPAD/../../LIB/STD_TYPES.h:
+
+../HAL/KPAD/../../LIB/BIT_MATH.h:
+
+../HAL/KPAD/../../MCAL/DIO/DIO_Interface.h:
+
+../HAL/KPAD/KPAD_Interface.h:
+
+../HAL/KPAD/KPAD_Private.h:
+
+../HAL/KPAD/KPAD_Config.h:
---
+++ protues/output/Debug/MCAL/DIO/DIO_Program.d
@@ -0,0 +1,14 @@
+MCAL/DIO/DIO_Program.d MCAL/DIO/DIO_Program.o: ../MCAL/DIO/DIO_Program.c \
+ ../MCAL/DIO/../../LIB/STD_TYPES.h ../MCAL/DIO/../../LIB/BIT_MATH.h \
+ ../MCAL/DIO/DIO_Interface.h ../MCAL/DIO/DIO_Private.h \
+ ../MCAL/DIO/DIO_Config.h
+
+../MCAL/DIO/../../LIB/STD_TYPES.h:
+
+../MCAL/DIO/../../LIB/BIT_MATH.h:
+
+../MCAL/DIO/DIO_Interface.h:
+
+../MCAL/DIO/DIO_Private.h:
+
+../MCAL/DIO/DIO_Config.h:
