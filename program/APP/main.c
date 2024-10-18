e80cb68fbd1367c8cc15ef0f840bdff0792f2549
MostafaSamirKamel <mostafasameer858@gmail.com>
2024-10-18 21:59:20 +0300

    delete unused file

--- program/APP/main.c
+++ program/APP/main.c
@@ -2,25 +2,16 @@
 #include "../LIB/STD_TYPES.h"
 #include "../LIB/BIT_MATH.h"
 #include "../MCAL/DIO/DIO_Interface.h"
-#include "../MCAL/TIMERS/TIMER0/TIMER0_Interface.h"
 #include "../HAL/LED/LED_Interface.h"
-#include "../MCAL/GIE/GIE_Interface.h"
+#include "../HAL/LCD/LCD_Interface.h"
 
-void ISR_OV(void);
+
 int main(void)
 {
-	GIE_void_GI_Enable(ON);
-	TIMER0_voidInit();
-	LED_voidInit_Led_Pin(PORTB,PIN0);
-	TIMER0_voidSetCallBack_OVF(ISR_OV,1000);
+
 	while(1)
 	{
 
 	}
 	return 0 ;
-}
-
-void ISR_OV(void)
-{
-	LED_voidToggle_Led_Pin(PORTB,PIN0);
 }
